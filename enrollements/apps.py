e340e99ab09c4df29a3bd9d3983d8213d0162a28
miniminimo <130142154+miiniminimo@users.noreply.github.com>
2025-09-25 22:10:30 +0900

    migrate update

--- enrollements/apps.py
+++ enrollements/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EnrollmentsConfig(AppConfig):
+class EnrollementsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'enrollments'
+    name = 'enrollements'
